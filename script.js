a26167ac9103e6f823b500b151b748dbf6ee42af
OilPainter1 <hjcryns@gmail.com>
2023-03-09 16:05:23 -0600

    added start button, configure button event listener

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let userHoursInput
+let userMinutesInput
+let userSecondsInput
+
 const spinner = $(".spinner").spinner({
     min:0,
     classes: {
@@ -6,8 +10,19 @@ const spinner = $(".spinner").spinner({
     }
 }).val(0)
 
+$("#start").prop("disabled","true")
+
 $("#reset").click((event)=>{
-    event.preventDefault()
     $(".spinner").val(0)
     return
 })
+
+
+$("#configure").click((event)=>{
+    userHoursInput = $("#HoursInput").val()
+    userMinutesInput = $("#MinutesInput").val()
+    userSecondsInput = $("#SecondsInput").val()
+    console.log(userHoursInput,userMinutesInput,userSecondsInput)
+})
+
+
