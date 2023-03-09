65876fb4655782ed650f3c1a939b0be598c25fb9
OilPainter1 <hjcryns@gmail.com>
2023-03-09 11:40:20 -0600

    implemented spinners

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const spinner = $(".selector").spinner({
+    min:0,
     classes: {
-        "ui-spinner": "highlight"
+        "ui-spinner": "highlight",
+        
     }
-})
+}).val(0).addClass("disabled")
