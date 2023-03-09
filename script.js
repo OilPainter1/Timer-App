282b81469e6c85d9cbd54f33c1c728351448c352
OilPainter1 <hjcryns@gmail.com>
2023-03-09 12:29:24 -0600

    added reset and configure button

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const spinner = $(".selector").spinner({
+const spinner = $(".spinner").spinner({
     min:0,
     classes: {
         "ui-spinner": "highlight",
         
     }
-}).val(0).addClass("disabled")
+}).val(0)
+
+$("#reset").click((event)=>{
+    event.preventDefault()
+    $(".spinner").val(0)
+    return
+})
