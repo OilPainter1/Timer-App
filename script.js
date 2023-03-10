46ab02b7de690036a22def7f315e6747069a68b6
OilPainter1 <hjcryns@gmail.com>
2023-03-09 20:26:50 -0600

    implemented timer countdown

--- script.js
+++ script.js
@@ -10,19 +10,47 @@ const spinner = $(".spinner").spinner({
     }
 }).val(0)
 
-$("#start").prop("disabled","true")
+$("#start").attr("disabled","true")
 
 $("#reset").click((event)=>{
+    if(!$("#start").prop("disabled")){
+        $("#start").attr("disabled","true")
+        $("#configure").removeAttr("disabled")
+    }
     $(".spinner").val(0)
     return
 })
+
+
 
 
 $("#configure").click((event)=>{
     userHoursInput = $("#HoursInput").val()
     userMinutesInput = $("#MinutesInput").val()
     userSecondsInput = $("#SecondsInput").val()
+    $("#configure").prop("disabled","true")
+    $("#start").removeAttr("disabled")
     console.log(userHoursInput,userMinutesInput,userSecondsInput)
+
+})
+
+$("#start").click((event)=>{
+    $("#start").attr("disabled","true")
+    var timeout = moment.duration({
+        seconds: userSecondsInput,
+        minutes: userMinutesInput,
+        hours: userHoursInput
+    })
+
+        setInterval(()=>{
+        timeout.subtract(1,"s")
+        $("#hoursLeft").text(timeout._data.hours)
+        $("#minutesLeft").text(timeout._data.minutes)
+        $("#secondsLeft").text(timeout._data.seconds)
+        console.log(timeout._data.hours,timeout._data.minutes,timeout._data.seconds)
+        },1000)
+    
+
 })
 
 
