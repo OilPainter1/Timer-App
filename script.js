4b50decd4fa6cb0017a7c5e10bc6df98e7b7ebc6
OilPainter1 <hjcryns@gmail.com>
2023-03-15 14:45:01 -0500

    added modal, fixed interval function

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
+
+
 let userHoursInput
 let userMinutesInput
 let userSecondsInput
+let timeout
+
+Disable(["start","stop"])
 
 const spinner = $(".spinner").spinner({
     min:0,
     classes: {
         "ui-spinner": "highlight",
-        
     }
 }).val(0)
 
-$("#start").attr("disabled","true")
-
-$("#reset").click((event)=>{
-    if(!$("#start").prop("disabled")){
-        $("#start").attr("disabled","true")
-        $("#configure").removeAttr("disabled")
-    }
-    $(".spinner").val(0)
-    return
-})
 
 function setTimer(hours,minutes,seconds){
     $("#timerHours").text(hours)
@@ -28,7 +22,41 @@ function setTimer(hours,minutes,seconds){
     return
 }
 
+function openModalResetButtons(timeout){
+    var modal = $("#modal")
 
+    modal.addClass("is-active")
+    $(".modal-background").click(()=>{
+        modal.removeClass("is-active")
+    })
+    $(".modal-close").click(()=>{
+        modal.removeClass("is-active")
+        
+    })
+    setTimer(timeout._data.hours,timeout._data.minutes,timeout._data.seconds)
+    Disable(["stop"])
+    $("#start").removeAttr("disabled")
+
+}
+
+function Disable(idArray){
+    for(var i =0; i<idArray.length;i++){
+        $(`#${idArray[i]}`).attr("disabled","true")
+    }
+    return
+}
+
+
+$("#reset").click((event)=>{
+    Disable(["start","stop"])
+
+    if($("#timerHours").val() || $("#timerMinutes").val() || $("#timerSeconds").val()){
+        setTimer(0,0,0)
+    }
+    $("#configure").removeAttr("disabled")
+    $(".spinner").val(0)
+    return
+})
 
 
 $("#configure").click((event)=>{
@@ -36,27 +64,36 @@ $("#configure").click((event)=>{
     userMinutesInput = $("#MinutesInput").val()
     userSecondsInput = $("#SecondsInput").val()
     setTimer(userHoursInput,userMinutesInput,userSecondsInput)
-    $("#configure").prop("disabled","true")
+    Disable(["configure"])
     $("#start").removeAttr("disabled")
     console.log(userHoursInput,userMinutesInput,userSecondsInput)
 
 })
 
 $("#start").click((event)=>{
-    $("#start").attr("disabled","true")
-    var timeout = moment.duration({
+    Disable(["start","reset"])
+
+    timeout = moment.duration({
         seconds: userSecondsInput,
         minutes: userMinutesInput,
         hours: userHoursInput
     })
+    var timeoutClone = timeout.clone()
 
-        setInterval(()=>{
+        var timerID= setInterval(()=>{
         timeout.subtract(1,"s")
         setTimer(timeout._data.hours,timeout._data.minutes,timeout._data.seconds)
-        console.log(timeout._data.hours,timeout._data.minutes,timeout._data.seconds)
+        if($("#timerHours").text() ==0 && $("#timerMinutes").text() == 0 &&
+        $("#timerSeconds").text() == 0){
+            openModalResetButtons(timeoutClone)
+            clearInterval(timerID) 
+            return
+        }
         },1000)
-    
-
 })
+
+$("#stop").click(console.log(userSecondsInput))
+
+
 
 
